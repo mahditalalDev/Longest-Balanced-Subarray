c59c2859609bd51544840dba9918bc269b54a96e
Mahdi Talal <mahditalal.789@gmail.com>
2024-11-03 20:22:48 +0200

    Add comments to clarify code logic

--- longest_balanced_subarray.py
+++ longest_balanced_subarray.py
@@ -1,11 +1,15 @@
+
 def find_longest_balanced_subarray(list):                           #O(1)
     counter_of_ones = 0                                             #O(1) 
     for i in range (len(list)):                                     #O(1) since len of list Constant  
         if list[i]==1:                                              #O(1)
             counter_of_ones +=list[i]                               #O(1)  
+    # Compare the count of ones to the count of zeros
     if counter_of_ones > len(list)-counter_of_ones:                 #O(1)
+            # Return the length of the zeros (balanced length)
             return( (len(list) - counter_of_ones ) %len(list))      #O(1)
     elif counter_of_ones < len(list)-counter_of_ones:               #O(1)
+            # Return the length of the zeros (balanced length)
             return (len(list) % (len(list) - counter_of_ones))      #O(1)
     else:                                                           #O(1)
             return counter_of_ones                                  #O(1)
