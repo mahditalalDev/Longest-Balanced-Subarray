f7554ea52bab509e52d780f31f934ca918ef1164
Mahdi Talal <mahditalal.789@gmail.com>
2024-11-03 20:13:43 +0200

    add the concept of bigO notation to my previous code

--- longest_balanced_subarray.py
+++ longest_balanced_subarray.py
@@ -1,13 +1,13 @@
-def find_longest_balanced_subarray(list):
-    counter_of_ones = 0
-    for i in range (len(list)):
-        if list[i]==1:
-            counter_of_ones +=list[i]
-    if counter_of_ones > len(list)-counter_of_ones:
-            return( (len(list) - counter_of_ones ) %len(list))
-    elif counter_of_ones < len(list)-counter_of_ones:
-            return (len(list) % (len(list) - counter_of_ones))
-    else:
-            return counter_of_ones
+def find_longest_balanced_subarray(list):                           #O(1)
+    counter_of_ones = 0                                             #O(1) 
+    for i in range (len(list)):                                     #O(1) since len of list Constant  
+        if list[i]==1:                                              #O(1)
+            counter_of_ones +=list[i]                               #O(1)  
+    if counter_of_ones > len(list)-counter_of_ones:                 #O(1)
+            return( (len(list) - counter_of_ones ) %len(list))      #O(1)
+    elif counter_of_ones < len(list)-counter_of_ones:               #O(1)
+            return (len(list) % (len(list) - counter_of_ones))      #O(1)
+    else:                                                           #O(1)
+            return counter_of_ones                                  #O(1)
 
-print(find_longest_balanced_subarray([1,1,1,0]))
+print(find_longest_balanced_subarray([1,1,1,0]))                    #O(1)
