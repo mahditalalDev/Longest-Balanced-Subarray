cf6e1a06a393ee63ec90dd93e6766c5129b6946f
Mahdi Talal <mahditalal.789@gmail.com>
2024-11-07 21:36:35 +0200

    add doc string for each  function

--- longest_balanced_subarray.py
+++ longest_balanced_subarray.py
@@ -1,25 +1,39 @@
+def read_lists_from_user():
+        """function to read list of 0s and 1s
 
-def find_longest_balanced_subarray(list):                           #O(1)
-    counter_of_ones = 0                                             #O(1) 
-    for i in range (len(list)):                                     #O(1) since len of list Constant  
-        if list[i]==1:                                              #O(1)
-            counter_of_ones +=list[i]                               #O(1)  
-    # Compare the count of ones to the count of zeros
-    if counter_of_ones > len(list)-counter_of_ones:                 #O(1)
-            # Return the length of the zeros (balanced length)
-            return( (len(list) - counter_of_ones ) %len(list)*2)      #O(1)
-    elif counter_of_ones < len(list)-counter_of_ones:               #O(1)
-            # Return the length of the zeros (balanced length)
-            return (len(list) % (len(list) - counter_of_ones)*2)      #O(1)
-    else:                                                           #O(1)
-            return counter_of_ones*2                                  #O(1)
+        Returns:
+            list: list of zeros and onces
+        """
+        user_list=input("Enter an list of (0s and 1s) separated by a space: ")
+        user_list=list(map(int,user_list.split()))
+        return user_list
 
-print(find_longest_balanced_subarray([1,1,0,0,0,0,1]))                    #O(1)
+def find_longest_balanced_subarray(list):
+        """function that return longest balanced array of zeros and onces
+
+        Args:
+            list (): list of zeros and onces
+
+        Returns:
+            list: balanced sub array of zeros and onces
+        """
+        counter_of_ones = 0                                         
+        for i in range (len(list)):                                   
+                if list[i]==1:                                       
+                        counter_of_ones +=list[i]                    
+        # Compare the count of ones to the count of zeros
+        if counter_of_ones > len(list)-counter_of_ones:              
+                # Return the length of the zeros (balanced length)
+                return( (len(list) - counter_of_ones ) %len(list)*2) 
+        elif counter_of_ones < len(list)-counter_of_ones:            
+                # Return the length of the zeros (balanced length)
+                return (len(list) % (len(list) - counter_of_ones)*2) 
+        else:                                                        
+                return counter_of_ones*2                             
 
 
-
-
-
+user_list=read_lists_from_user()
+print(find_longest_balanced_subarray(user_list))
 
 # def second_solution(list):
 #         zeros_counter=0
